196f4001470da048f0734423a612142fc9aa9eb3
heinkhantzaw <hein@engineerforce.io>
2024-03-08 15:36:41 +0000

    make it prettier

--- service-worker.js
+++ service-worker.js
@@ -4,10 +4,10 @@ self.addEventListener("install", () => {
   console.log("[ServiceWorker] Installed");
 });
 
-self.addEventListener("fetch", function(e) {
+self.addEventListener("fetch", function (e) {
   if (e.request.url.startsWith("http") || e.request.url.startsWith("https")) {
     e.respondWith(
-      caches.match(e.request).then(function(cachedFile) {
+      caches.match(e.request).then(function (cachedFile) {
         if (cachedFile) {
           console.log(
             "[Service Worker] Resource fetched from the cache for: " +
@@ -15,8 +15,8 @@ self.addEventListener("fetch", function(e) {
           );
           return cachedFile;
         } else {
-          return fetch(e.request).then(function(response) {
-            return caches.open(cacheName).then(function(cache) {
+          return fetch(e.request).then(function (response) {
+            return caches.open(cacheName).then(function (cache) {
               cache.put(e.request, response.clone());
               console.log(
                 "[Service Worker] Resource fetched and saved in the cache for: " +
